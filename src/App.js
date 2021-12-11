ee0093ef8880b3dcb4990043b04150bb5fd60c3b
Denver-Meadows <meadows8200@gmail.com>
2021-12-11 17:54:11 -0500

    updated styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 // import Switch from "react-router-dom";
 // import Route from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
-  return <Navbar />;
+  return (
+    <main>
+      <Navbar />
+      <Header />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <section className="header">
+      <div className="header-content">
+        <h1>denver meadows</h1>
+        <h2>front end web developer</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
