a06bf086371c297ba7d747a1d04143a198521727
Denver-Meadows <meadows8200@gmail.com>
2021-12-11 16:43:38 -0500

    styling nav and body

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+// import Switch from "react-router-dom";
+// import Route from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <h1>My Portfolio</h1>;
+  return <Navbar />;
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav className="nav">
+      <ul className="nav-list">
+        {/* turn to anchor tags */}
+        <li className="nav-list-link">Home</li>
+        <li className="nav-list-link">Projects</li>
+        <li className="nav-list-link">About</li>
+        <li className="nav-list-link">Contact</li>
+        <li className="nav-list-link">Resume</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
