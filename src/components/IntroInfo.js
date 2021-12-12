bdd7a6215c87aeb0b917f62de3ceba43d51997e7
Denver-Meadows <meadows8200@gmail.com>
2021-12-12 17:21:23 -0500

    Updating info section

---
+++ src/components/IntroInfo.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const IntroInfo = () => {
+  return (
+    <div className="intro">
+      <p className="intro-info-1">Hello,</p>
+      <p className="intro-info-2">
+        I'm a software engineer that specializes in frontend web development,
+        currently based in Cincinnati, OH.
+      </p>
+      <div className="intro-btn-container">
+        <button className="btn intro-btn">See my work</button>
+      </div>
+    </div>
+  );
+};
+
+export default IntroInfo;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import BGvideo from "../components/BGvideo";
+import IntroInfo from "../components/IntroInfo";
 
 const Home = () => {
   return (
     <main>
       <Header />
       <BGvideo />
+      <IntroInfo />
     </main>
   );
 };
