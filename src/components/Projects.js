c0df2baf582202f3055585270312d045f3df59ba
Denver-Meadows <meadows8200@gmail.com>
2021-12-15 21:29:03 -0500

    ready for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Projects = () => {
+  // create a project component
+  // create the data for each project
+  // loop through that data to display it
   return (
     <section className="projects">
       <h1 className="projects-header">Projects</h1>
-      <div className="underline"></div>
+      <div className="projects-single"></div>
     </section>
   );
 };
