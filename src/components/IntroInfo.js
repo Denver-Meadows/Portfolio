f1bafdf10f7476752145584b8ea34c45362ab5f8
Denver-Meadows <meadows8200@gmail.com>
2021-12-19 12:38:27 -0500

    update to projets and styling

--- src/components/IntroInfo.js
+++ src/components/IntroInfo.js
@@ -6,8 +6,7 @@ const IntroInfo = () => {
       <p className="intro-info-1">Hello,</p>
       <p className="intro-info-2">
         I'm a software engineer that specializes in frontend web development,
-        currently based in Cincinnati, OH. Please keep scrolling to see projects
-        I've worked on.
+        currently based in Cincinnati, OH.
       </p>
       <div className="down-arrow">
         <div className="chevron"></div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,18 @@
 import React from "react";
 import SingleProject from "./SingleProject";
 import { projectData } from "../data/project-data";
+import photo1 from "../imgs/comphoto.jpg";
 
 const Projects = () => {
   return (
     <section className="projects">
+      <div className="projects-photo">
+        <img
+          className="projects-photo-1"
+          src={photo1}
+          alt="pixel of progammers working"
+        />
+      </div>
       <h1 className="projects-header">Projects</h1>
       <React.Fragment>
         {projectData.map((project, index) => {
