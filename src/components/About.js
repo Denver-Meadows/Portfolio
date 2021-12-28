ec5697b25070941f7fd931681a073b34d9b08c99
Denver-Meadows <meadows8200@gmail.com>
2021-12-28 18:32:53 -0500

    experience updated.  ready for styling

--- src/components/About.js
+++ src/components/About.js
@@ -6,15 +6,35 @@ const About = () => {
       <h1 className="about-header">Experience</h1>
       <div className="about-container">
         <div className="about-container-content">
-          <p className="about-container-content-description">
-            I'm a software engineer that specializes in front-end web
-            development. Currently based in the Cincinnati, OH area.
-          </p>
+          <div className="about-container-content-description">
+            <p>
+              Hello! My name is Denver and I love creating things that live on
+              the internet. My interest in web development started back in 2018
+              when I was first introduced to Python. At that time I was using it
+              to scrape the web, build games and other fun small projects.{" "}
+            </p>
+            <p>
+              From there I made my way to the world of web development and found
+              a passion for building fun interactive web apps. Since then I have
+              been immersed in Javascript, React and all things front-end!
+            </p>
+            <p>
+              {" "}
+              I'm currently available for exciting new opportunities and would
+              love to connect.
+            </p>
+          </div>
           <div className="about-container-content-tech">
-            <h2>Technologies</h2>
+            <h3>Technologies I Enjoy Building With</h3>
             <ul>
-              <li>Javascript</li>
-              <li>SASS</li>
+              <li>JavaScript (ES6+)</li>
+              <li>React</li>
+              <li>Express</li>
+              <li>Node.JS</li>
+              <li>SCSS / SASS</li>
+              <li>Mongoose</li>
+              <li>Git / GitHub</li>
+              <li>Python</li>
             </ul>
           </div>
         </div>
