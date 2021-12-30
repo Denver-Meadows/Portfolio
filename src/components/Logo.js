c39bdcea9df73190828444854a2e4b9d3af7d522
Denver-Meadows <meadows8200@gmail.com>
2021-12-30 17:11:32 -0500

    navbar updates

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="outside-logo">
+      <div className="inside-logo">D</div>
+    </div>
+  );
+};
+
+export default Logo;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,26 @@
 import React from "react";
 import "../styles/nav.scss";
+import "./Logo";
+import Logo from "./Logo";
 
 const Navbar = () => {
   return (
     <nav className="nav">
+      <Logo />
       <ul className="nav-list">
         {/* turn to anchor tags */}
-        <li className="nav-list-link nav-list-link-1 ">Home</li>
-        <li className="nav-list-link nav-list-link-2">Experience</li>
-        <li className="nav-list-link nav-list-link-3">Work</li>
-        <li className="nav-list-link nav-list-link-4">Contact</li>
-        <li className="nav-list-link nav-list-link-5">Resume</li>
+        <li className="nav-list-link nav-list-link-2">
+          <span className="nav-number">01.</span> Experience
+        </li>
+        <li className="nav-list-link nav-list-link-3">
+          <span className="nav-number">02.</span> Work
+        </li>
+        <li className="nav-list-link nav-list-link-4">
+          <span className="nav-number">03.</span> Contact
+        </li>
+        <li className="nav-list-link nav-list-link-5">
+          <span className="nav-number">04.</span> Resume
+        </li>
       </ul>
     </nav>
   );
