00786d96f3d47256e6c482937cb063889d506823
Denver-Meadows <meadows8200@gmail.com>
2022-01-16 14:22:44 -0500

    updating animations

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -3,10 +3,11 @@ import { BsArrowUp } from "react-icons/bs";
 
 const ScrollButton = () => {
   const [visible, setVisible] = useState(false);
+  console.log(window.innerWidth);
 
   const toggleVisible = () => {
     const scrolled = document.documentElement.scrollTop;
-    if (scrolled > 500) {
+    if (scrolled > 500 && window.innerWidth > 600) {
       setVisible(true);
     } else if (scrolled <= 500) {
       setVisible(false);
